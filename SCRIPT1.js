e89db90137ec35eeb601b041307394950c4a4d76
JoseLD96 <75328762@certus.edu.pe>
2025-04-25 22:05:26 -0500

    2025v8

---
+++ SCRIPT1.js
@@ -0,0 +1,23 @@
+function FCambioColor(){
+
+   let vtexto1 = document.getElementById("ejmDOM1");
+   vtexto1.texContent= "texto cambiado por funcion FCamboiColor";
+   vtexto1.style.color="red";
+   vtexto1.insertAdjacentHTML("afterend",
+                               "<br>Eset texto es adicional <br>");
+   vtexto1.style.color="green";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
