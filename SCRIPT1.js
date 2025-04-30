80372f41bbd692cd099a34dac51ed26ffd807229
JoseLD96 <75328762@certus.edu.pe>
2025-04-29 21:57:48 -0500

    2025v5.3

--- SCRIPT1.js
+++ SCRIPT1.js
@@ -1,20 +1,34 @@
-function FCambioColor(){
+let varGloball1="Texto de variable global";
 
-   let vtexto1 = document.getElementById("ejmDOM1");
-   vtexto1.texContent= "texto cambiado por funcion FCamboiColor";
-   vtexto1.style.color="red";
-   vtexto1.insertAdjacentHTML("afterend",
-                               "<br>Eset texto es adicional <br>");
-   vtexto1.style.color="green";
+function FCambioColor(){   
+    letvar.Temporal1= "Texto temporal";
+    letvTexto1=document.getElementById("ejmDOM1");
+    vTexto1.textContent="Texto cambiado por FCambioColor()";
+    vTexto1.style.color="red";
+    vTexto1.insertAdjacentHTML("beforeEnd",
+                            "<br>este texto es adicional <br><br>");
+   vTexto1.style.backgroundColor="green";
+   console.log{vTexto1.innerText}
+    vTexto1.insertAdjacentHTML("beforeEnd",varGloball1);
+    //La variable global se puede utilizar dentro de cualquier funcion
+
+}
+vTexto1= "Texto fuera de funcion"; //vtexto1 no es la variable de la funcion
+console.log(vTexto1);
+console.log(varTemporal1); //varTemporal no esta definido afuer de funcion
+
+unction FCambioClase1() {
+    let vTexto2 = document.getElementsByClassName("classDOM1");
+    vTexto2[1].innerHTML = "Elemento [1] del arreglo de clase classDOM1";
+    console.log("Tamaño del array=", vTexto2.length);
+    // se coloca el índice 1 por ser el 2do elemento de la clase classDOM1 de la pag web
 }
 
-
-
-
-
-
-
-
+function FCambioTag1() {
+    let vTexto3 = document.getElementsByTagName('h2');
+    vTexto3[6].innerHTML = "Elemento h2 cambiado por TagDOM1";
+    // se coloca el índice 6 por ser el 7mo elemento h2 de la pag web index.html
+}
 
 
 
