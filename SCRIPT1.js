337bcbeacd8081a4333420e5ba9c04b2a2217b4f
JoseLD96 <75328762@certus.edu.pe>
2025-05-06 21:52:35 -0500

    20265 V5.4

--- SCRIPT1.js
+++ SCRIPT1.js
@@ -1,37 +1,59 @@
-let varGloball1="Texto de variable global";
+function FUsoFor1()
+{
+  const vTextoClass = document.getElementsByClassName("classFOR1");
+  var vTextoTotal="";
+  // como length=4 --> ira desde 0 hasta 3
+  for(let i=0 ; i< vTextoClass.length ; i++ )
+  {
+    vTextoClass[i].style.border ="2px solid red" ;
+    vTextoTotal=vTextoTotal + vTextoClass[i].innerText+'-';
 
-function FCambioColor(){   
-    letvar.Temporal1= "Texto temporal";
-    letvTexto1=document.getElementById("ejmDOM1");
-    vTexto1.textContent="Texto cambiado por FCambioColor()";
+  } // fin de for
+  var nuevoH1 =document.createElement("h1");
+  var texto = document.createTextNode(vTextoTotal);
+  nuevoH1.appendChild(texto);
+  vTextoClass[3].appendChild(nuevoH1);
+  
+}  // fin de function
+
+function FEliminarConcat()
+{
+  const vTextoClass = document.getElementsByClassName("classFOR1");
+  let rpta =prompt("Estas seguro de eliminar el texto concatenado?(S/N)",'');
+  if (rpta=="S" || rpta=="s")  //  doble || significa o
+    { vTextoClass[3].remove();  // eliminar la class de posicion [3]
+      alert("CONCATENCION ELMINADA");
+    }  
+  else
+    { if(rpta=="N" || rpta=="n")
+      { alert("NO SE elimnará");
+      }
+      else
+      { alert("OPCION INCORRECTA-INgresa nuevamente");
+      }
+    }
+  //removeChild funciona cuando un nodo(class, id, etx) esta dentro de otro
+}
+
+function FCapturarNombre(){
+
+    const vTexto1 = document.getElementById("nombre");
+    const vTexto2 = document.querySelectorAll("input.datosPersonales");
+    const vTexto3 = document.getElementById("clave");
+
+   // usar input para clases relacioandas a cajas de texto, etc
     vTexto1.style.color="red";
-    vTexto1.insertAdjacentHTML("beforeEnd",
-                            "<br>este texto es adicional <br><br>");
-   vTexto1.style.backgroundColor="green";
-   console.log{vTexto1.innerText}
-    vTexto1.insertAdjacentHTML("beforeEnd",varGloball1);
-    //La variable global se puede utilizar dentro de cualquier funcion
+    
+    var valor1 =vTexto1.value; // value obtiene el contenido de la caja de texto nombre
+    var valor2 =""; //tipo string 
+    vTexto3.value=valor1; //asigna el nombre a la clave
+  
+    //foreach recorre todo el contenido del array de la clase datosPersonales
+    vTexto2.forEach(input => {
+        valor2 =valor2 +  `${input.name}: ${input.value}       \n`;  // alt+96 
+      //para comillas tipotexto permiten colocar variables del form y convertirlas a texto mediante $
+      });
 
+    document.getElementById('observacion').value = valor2;
+    console.log('El valor del nombre es:', valor1);
 }
-vTexto1= "Texto fuera de funcion"; //vtexto1 no es la variable de la funcion
-console.log(vTexto1);
-console.log(varTemporal1); //varTemporal no esta definido afuer de funcion
-
-unction FCambioClase1() {
-    let vTexto2 = document.getElementsByClassName("classDOM1");
-    vTexto2[1].innerHTML = "Elemento [1] del arreglo de clase classDOM1";
-    console.log("Tamaño del array=", vTexto2.length);
-    // se coloca el índice 1 por ser el 2do elemento de la clase classDOM1 de la pag web
-}
-
-function FCambioTag1() {
-    let vTexto3 = document.getElementsByTagName('h2');
-    vTexto3[6].innerHTML = "Elemento h2 cambiado por TagDOM1";
-    // se coloca el índice 6 por ser el 7mo elemento h2 de la pag web index.html
-}
-
-
-
-
-
-
